78d8a8531ade85b05acf9713606dbd170b24fff6
MarioPadilla016 <padillamario078@gmail.com>
2023-09-02 01:17:22 -0600

    Guia 1
    Entrega Guia 1( 2 x Ejercicios)

---
+++ Guia 1/Ejercicio 1/EjemploJavaSwim/src/main/java/com/mycompany/ejemplojavaswim/EjemploJavaSwim.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplojavaswim;
+
+/**
+ *
+ * @author padil
+ */
+public class EjemploJavaSwim {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
